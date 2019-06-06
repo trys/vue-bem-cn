448ffe2276b144fb9afb9e7da50b3cc2d00c87e4
Trys Mudford <trysmudford@gmail.com>
2019-06-06 11:05:34 +0100

    Remove constructor check

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,6 @@ export const isPObject = val =>
   val &&
   typeof val === 'object' &&
   val !== null &&
-  val.constructor === Object &&
   Object.keys(val).length > 0;
 
 export const hyphenate = str => str.replace(/\B([A-Z])/g, '-$1').toLowerCase();
